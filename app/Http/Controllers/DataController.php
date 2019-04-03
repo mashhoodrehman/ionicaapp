0cbe425d6bbcf1e65869a2dd4fc86d451fe717c4
mashhood <mashhoodr.rehman@gmail.com>
2019-04-04 00:11:20 +0500

    sdfsdfsdf

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -34,7 +34,10 @@ class DataController extends Controller
                 $images->image = $value;
                 $images->save();
                 }
-
+                $categoryevent = new Category_Event;
+                $categoryevent->event_id = $event->id;
+                $categoryevent->category_id = $request->category;
+                $categoryevent->save();
                 return response()->json(['message' => 'added event' , 'code' => 200]);
 
             }
