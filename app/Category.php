563865a664cb7b89b4ba83d84f36057c77b631b4
mashhood <mashhoodr.rehman@gmail.com>
2019-04-06 11:36:44 +0500

    sdfsdfsdf

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'categories';
+    protected $table = 'categories';\
+
+    protected $primary = 'category_id';
 
     public function events(){
     	return $this->belongsToMany('App\Category' , 'categories_events' , 'event_id' , 'category_id');
